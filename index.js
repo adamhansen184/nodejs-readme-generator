bd4c31be34e0c672e271eeea827365734001e91b
Adam Hansen <adamhansen@me.com>
2023-06-15 17:50:24 -0600

    Create and link issue numbers to original comments
    found in starter code from https://github.com/coding-boot-camp/potential-enigma

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// TODO: Include packages needed for this application
+// TODO: #23 Include packages needed for this application
 
-// TODO: Create an array of questions for user input
+// TODO: #24 Create an array of questions for user input
 const questions = [];
 
-// TODO: Create a function to write README file
+// TODO: #25 Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
+// TODO: #26 Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
