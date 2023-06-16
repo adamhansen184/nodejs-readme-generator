123e24ccc22bb29dda8eb4eeae12eeae06af586a
Adam Hansen <adamhansen@me.com>
2023-06-15 18:25:30 -0600

    Add comments and link issues
    #1 #3 #5 #7 #9 #11 #13 #16 #18

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 // TODO: #23 Include packages needed for this application
 
 // TODO: #24 Create an array of questions for user input
-const questions = [];
+const questions = [
+    // TODO: #1 Prompt for project title
+
+    // TODO: #3 Prompt for project description
+
+    // TODO: #5 Prompt for project installation instructions
+
+    // TODO: #7 Prompt for project usage information
+
+    // TODO: #9 Prompt for project contribution guidelines
+
+    // TODO: #11 Prompt for project test instructions
+
+    // TODO: #13 Prompt for project license
+
+    // TODO: #16 Prompt for GitHub username
+
+    // TODO: #18 Prompt for email address
+    
+];
 
 // TODO: #25 Create a function to write README file
 function writeToFile(fileName, data) {}
