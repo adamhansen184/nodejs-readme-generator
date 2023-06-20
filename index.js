a4e1303a1f57c1905992e6d32be076960d4c0801
Adam Hansen <adamhansen@me.com>
2023-06-20 14:10:28 -0600

    Import fs, generateMarkdown, and inquirer packages

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // TODO: #23 Include packages needed for this application
+import * as fs from 'node:fs';
+import * as generateMarkdown from './utils/generateMarkdown.mjs';
+import * as inquirer from 'inquirer';
 
 // TODO: #24 Create an array of questions for user input
 const questions = [
